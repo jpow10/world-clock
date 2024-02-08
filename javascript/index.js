77f451f72160600f24c8ac849117cc9f1a304368
jpow10 <juliapowney@hotmail.co.uk>
2024-02-08 12:20:12 +0000

    Added JS interval

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,26 @@
-let berlinElement = document.querySelector("#berlin");
-berlinElement.innerhtml = "hello";
+function updateTime() {
+  // Berlin
+
+  let berlinElement = document.querySelector("#berlin");
+  let berlinDateElement = berlinElement.querySelector(".date");
+  let berlinTimeElement = berlinElement.querySelector(".time");
+  let berlinTime = moment().tz("Europe/Berlin");
+
+  berlinDateElement.innerHTML = berlinTime.format("dddd Do MMMM");
+  berlinTimeElement.innerHTML = berlinTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Tokyo
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("dddd Do MMMM");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
